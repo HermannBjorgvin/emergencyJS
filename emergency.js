64c3488df1a844a138167241a9684520cd3d2e05
HermannBjorgvin <hermann3646@gmail.com>
2014-10-06 20:02:05 +0000

    Emergency.js
    01189998819991197253

---
+++ emergency.js
@@ -0,0 +1,13 @@
+/*
+	From today, dialling 999 won’t get you the emergency services. 
+	And that’s not the only thing that’s changing. Nicer ambulances, 
+	faster response times and better-looking drivers mean they’re not 
+	just “the” emergency services - they’re “your” emergency services. 
+	So, remember the new number: 0118 999 881 999 119 725 ...3
+*/
+
+Number.emergency = function(){
+	return 01189998819991197253;
+}
+
+// Code (albeit not the most complex) by: Hermann.is
