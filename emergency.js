6d983b952ffdb04ff85060f92b37aee488463595
HermannBjorgvin <hermann3646@gmail.com>
2014-10-06 20:57:13 +0000

    Idk what else I can do.
    ...

--- emergency.js
+++ emergency.js
@@ -1,8 +1,8 @@
 /*
-	From today, dialling 999 won’t get you the emergency services. 
-	And that’s not the only thing that’s changing. Nicer ambulances, 
-	faster response times and better-looking drivers mean they’re not 
-	just “the” emergency services - they’re “your” emergency services. 
+	From today, dialling 999 won’t get you the emergency services.
+	And that’s not the only thing that’s changing. Nicer ambulances,
+	faster response times and better-looking drivers mean they’re not
+	just “the” emergency services - they’re “your” emergency services.
 	So, remember the new number: 0118 999 881 999 119 725 ...3
 */
 
